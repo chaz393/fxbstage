10374860b0e43b3d5624a7396d67e9fd557616d7
Chaz Lakinger <clakinger@gmail.com>
2023-12-24 15:56:29 -0600

    Bugfix 003 fix newline characters in post body (#6)
    remove newline characters in post text

--- src/bstage_embed_server.py
+++ src/bstage_embed_server.py
@@ -123,7 +123,7 @@ def get_photo_post_metadata(response):
 
     post_id = str(response.text).split("\"post\":{\"id\":\"")[1].split("\"")[0]
     author = str(response.text).split("\"nickname\":\"")[1].split("\"")[0]
-    post_text = str(response.text).split("\"body\":\"")[1].split("\"")[0]
+    post_text = str(response.text).split("\"body\":\"")[1].split("\"")[0].replace("\\n", "")
 
     media_ids = []
     image_url_dictionary = {}
@@ -144,7 +144,7 @@ def get_video_post_metadata(response):
     post_id = str(response.text).split("\"post\":{\"id\":\"")[1].split("\"")[0]
     media_id = str(response.text).split("\"video\":{\"id\":\"")[1].split("\"")[0]
     author = str(response.text).split("\"nickname\":\"")[1].split("\"")[0]
-    post_text = str(response.text).split("\"body\":\"")[1].split("\"")[0]
+    post_text = str(response.text).split("\"body\":\"")[1].split("\"")[0].replace("\\n", "")
     video_url = str(response.text).split("\"dashPath\":\"")[1].split("\"")[0]
 
     return BstagePost(PostType.VideoPost, post_id, post_text, [media_id], author, video_url=video_url)
